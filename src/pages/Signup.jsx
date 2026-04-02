eeaf3f98e407b55d348ec6607f85d976d3188ffd
Bappa <bappahalder2211@gmail.com>
2026-04-02 17:13:55 +0530

    login error fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,14 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { registerUser } from "../features/userSlice";
 import { yupResolver } from "@hookform/resolvers/yup";
-import {  signupSchema } from "../validation/userSchema";
+import { userSchema } from "../validation/userSchema";
 import toast from "react-hot-toast";
 
 const Signup = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(signupSchema) });
+  const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(userSchema) });
   const { loading, error } = useSelector((state) => state.user);
 
   const onSubmit = async (data) => {
@@ -39,50 +39,50 @@ const Signup = () => {
       <div className="px-4 h-dvh flex justify-center items-center bg-[url(./assets/signup_bg.webp)] bg-cover bg-center">
         <div className="border bg-[#00000087] p-6 rounded w-full max-w-[400px]">
           <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data" className="flex flex-col gap-3 ">
-          <div className="flex flex-col">
-            <input
-              type="text"
-              {...register("userName")}
-              placeholder="Username"
-              className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
-            />
-            <p className="error">{errors.userName?.message}</p>
-          </div>
-          <div>
-            <input
-              type="text"
-              {...register("phone")}
-              placeholder="Phone"
-              className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
-            />
-            <p className="error">{errors.phone?.message}</p>
-          </div>
-          <div className="flex flex-col">
-            <input
-              type="email"
-              {...register("email")}
-              placeholder="Email"
-              className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
-            />
-            <p className="error">{errors.email?.message}</p>
-          </div>
-          <div className="flex flex-col">
-            <input
-              type="password"
-              {...register("password")}
-              placeholder="Password"
-              className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
-            />
-            <p className="error">{errors.password?.message}</p>
-          </div>
-          <div className="flex flex-col">
-            <input
-              type="file"
-              {...register("avatar")}
-              className="text-sm text-white"
-            />
-            <p className="error">{errors.avatar?.message}</p>
-          </div>
+            <div className="flex flex-col">
+              <input
+                type="text"
+                {...register("userName")}
+                placeholder="Username"
+                className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
+              />
+              <p className="error">{errors.userName?.message}</p>
+            </div>
+            <div>
+              <input
+                type="text"
+                {...register("phone")}
+                placeholder="Phone"
+                className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
+              />
+              <p className="error">{errors.phone?.message}</p>
+            </div>
+            <div className="flex flex-col">
+              <input
+                type="email"
+                {...register("email")}
+                placeholder="Email"
+                className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
+              />
+              <p className="error">{errors.email?.message}</p>
+            </div>
+            <div className="flex flex-col">
+              <input
+                type="password"
+                {...register("password")}
+                placeholder="Password"
+                className="py-2 ps-2 border rounded bg-transparent text-sm outline-none text-white placeholder:text-white"
+              />
+              <p className="error">{errors.password?.message}</p>
+            </div>
+            <div className="flex flex-col">
+              <input
+                type="file"
+                {...register("avatar")}
+                className="text-sm text-white"
+              />
+              <p className="error">{errors.avatar?.message}</p>
+            </div>
 
             <button type="submit" className="py-2 bg-indigo-500 hover:bg-indigo-600 transition duration-300 ease-in-out text-white rounded">
               {loading ? "Registering..." : "Register"}
