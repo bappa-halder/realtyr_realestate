0cf73611069706475d94c0d7574ee2d6f11df172
Bappa <bappahalder2211@gmail.com>
2026-05-14 14:02:39 +0530

    add eye flash

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { registerUser } from "../features/userSlice";
 import toast from "react-hot-toast";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import usePasswordToggle from "../hooks/usePasswordToggle";
 
 const Signup = () => {
